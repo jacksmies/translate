8b7278b103981eca5ea3eb4297f55a487212739f
Jack Smies <jacksmies@gmail.com>
2020-03-30 17:12:48 +0400

    first test

---
+++ lang/en/text.php
@@ -0,0 +1,5 @@
+<?php
+$LANG['intro1'] = 'Welcome guys. It would be great if you can translate this.';
+$LANG['intro2'] = 'Just click on the suggest button if you agree with the machine translation';
+$LANG['intro3'] = 'Otherwise make some changes and submit them';
+$LANG['intro4'] = 'This is the end of the tutorial. Happy translating';
